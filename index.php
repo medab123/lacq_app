e0d6fb92f2af61f083152b3567232c7dcf3d1c19
elabidi mohammed <mohammed.elabidi123@gmail.com>
2022-03-02 07:40:58 +0000

    fix bug with proxy azur

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+echo '';
+//exit();
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
