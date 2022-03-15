cbb064350ecf1dedb263199b793291ce27c32e58
Elabidi Mohammed <mohammed.elabidi123@gmail.com>
2022-03-15 16:31:16 +0100

    update 15/03/22

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -30,7 +30,7 @@ class UserController extends Controller
     }
     public function index(Request $request)
     {
-        $data = User::orderBy('id','DESC')->paginate(5);
+        $data = User::orderBy('id','DESC');
         return view('users.index',compact('data'));
     }
 
