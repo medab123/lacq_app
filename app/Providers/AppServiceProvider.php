488f715b4f29380a330ae22cff15c5fd9c32fc99
Mohammed Elabidi <mohammed.elabidi123@gmail.com>
2023-03-09 16:00:46 +0000

    ser

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*\URL::forceRootUrl(\Config::get('app.url'));
+        \URL::forceRootUrl(\Config::get('app.url'));
         if (\Str::contains(\Config::get('app.url'), 'https://')) {
             \URL::forceScheme('https');
-        }*/
+        }
     }
 }
